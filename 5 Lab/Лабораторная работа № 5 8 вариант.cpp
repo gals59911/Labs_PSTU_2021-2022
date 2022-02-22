b708c5f075e56c3945ecc21ce67103f3aa6a4baf
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2022-02-22 15:25:14 +0500

    Add files via upload

---
+++ 5 Lab/Лабораторная работа № 5 8 вариант.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	const int s = 2;
+	bool flag = false;
+	int arr[s][s];
+	for (int i = 0; i < s; i++)
+	{
+		for (int g = 0; g < s; g++)
+		{
+			cin >> arr[i][g];
+		}
+	}
+		for (int i = 0; i < s; i++)
+		{
+			for (int g = 0; g < s; g++)
+			{
+				cout << arr[i][g] << "\t";
+			}
+			cout << endl;
+		}
+			for (int i = 0; i < s-1; i++)
+			{
+				for (int g = i+1; g < s; g++)
+				{
+					int str = arr[i][g];
+					arr[i][g] = arr[g][i];
+					arr[g][i] =str;
+
+				}
+			}
+	
+	cout << endl;
+	for (int i = 0; i <s; i++)
+	{
+		for (int g = 0; g < s; g++)
+		{
+			cout << arr[i][g] << "\t";
+
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < s; i++)
+	{
+		for (int g = 0; g < s; g++)
+		{
+			if (arr[i][g] != arr[g][i])
+			{
+				flag = true;
+			}
+		}
+	}
+	if (flag==true)
+	{
+		cout << "Матрица не симмтерична";
+	}
+	else { cout << "Матрица симметрична"; }
+}	
