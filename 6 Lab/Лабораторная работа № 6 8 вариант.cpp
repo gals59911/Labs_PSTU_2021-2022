d289b9ad61dc26f1b53ebd60105c4a2b7b9d04fc
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2022-02-22 18:06:32 +0500

    Add files via upload

---
+++ 6 Lab/Лабораторная работа № 6 8 вариант.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+	char s[] = "2321391719";
+	char t;
+	for (int j = 1; j < strlen(s); j++)
+	{
+		for (int i = 0; i < strlen(s); i++)
+		{
+			if (s[i] <= s[i + 1])
+			{
+				t = s[i + 1];
+				s[i + 1] = s[i];
+				s[i] = t;
+			}
+		}
+	}
+	for (int i = 0; i < strlen(s); i++)
+	{
+		cout << s[i]<<" ";
+	}
+
+}
