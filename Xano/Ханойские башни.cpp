d6c78534212ba3da5eb9e1728d8cc22bd3c6815e
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2022-02-22 16:19:07 +0500

    Add files via upload

---
+++ Xano/Ханойские башни.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int hano(int n, int st, int o, int v)
+{
+	if (n > 0)
+	{
+		hano(n - 1, st, v, o);
+		cout << st << "=>" << o << endl;
+		hano(n - 1, v, o, st);
+	}
+	return 0;
+}
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n;
+	cin >> n;
+	hano(n, 1, 3, 2);
+
+}
