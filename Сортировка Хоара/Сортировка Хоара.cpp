e0d65d4f79d4708d858bd9c11ba9a857246c9ec8
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2022-02-27 19:41:55 +0500

    Add files via upload

---
+++ Сортировка Хоара/Сортировка Хоара.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+const int n = 5;
+int arr[n];
+void sort(int arr[], int L, int R)
+{
+	int i, j, x, tmp;
+	i = L;
+	j = R;
+	x = arr[(L + R) / 2];
+	do
+	{
+		while (arr[i] < x) { i++; }
+		while (x < arr[j]) { j--; }
+		if (i <= j)
+		{
+			if (i < j) { tmp = arr[i]; arr[i] = arr[j]; arr[j] = tmp; }
+			i++;
+			j--;
+		}
+	} while (i <= j);
+	if (L < j)  sort(arr, L, j);
+	if (i < R)  sort(arr, i, R);
+}
+void v(int arr[])
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+}
+int main()
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand();
+	}
+	sort(arr, 0, n - 1);
+	v(arr);
+}
