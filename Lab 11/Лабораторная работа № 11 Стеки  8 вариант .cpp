0c51da858ced2d559601b04d78b04a2271e59ffe
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2022-03-12 20:02:45 +0500

    Add files via upload

---
+++ Lab 11/Лабораторная работа № 11 Стеки  8 вариант .cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	stack <char> s1;
+	stack <char> s2;
+	int n,c=0;
+	char sim;
+	cout << "Введите количество элементов: ";
+	cin >> n;
+	for (int i = 1;i<=n;i++)
+	{
+		cin >> sim;
+		s1.push(sim);
+		if (i % 2 != 0)
+		{
+			s2.push(sim);
+			c++;
+		}
+	}
+	for (int i=0; i <c; i++)
+	{
+		s1.push(s2.top());
+		s2.pop();
+	}
+	while (!s1.empty())
+	{
+		cout << s1.top() << " ";
+		s1.pop();
+	}
+
+}
