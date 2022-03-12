6881a2461448470af3de45aad9c64ba4b559f653
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2022-03-12 18:16:39 +0500

    Add files via upload

---
+++ Lab 11/Лабораторная работа № 11 Двунаправленный список  8 вариант .cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node* p_p = nullptr;
+	Node* p_n = nullptr;
+};
+
+struct List
+{
+	Node* head = nullptr;
+	Node* tail = nullptr;
+};
+
+void pushBack(List& list, int& data)
+{
+	Node* newnode = new Node;
+	newnode->data = data;
+	if (list.head == nullptr)
+	{
+		list.head = newnode;
+		list.tail = newnode;
+	}
+	else
+	{
+		newnode->p_p = list.tail;
+		list.tail->p_n = newnode;
+		list.tail = newnode;
+	}
+
+}
+
+void printList(List& list)
+{
+	Node* currentnode = list.head;
+	while (currentnode != nullptr)
+	{
+		cout << currentnode->data << " ";
+		currentnode = currentnode->p_n;
+	}
+	cout << endl;
+}
+
+void del(Node* d)
+{
+	d->p_p->p_n = d->p_n;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	List list;
+	int n, ch,c=0;
+	cout << "Введите размер списка: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> ch;
+		pushBack(list, ch);
+	}
+	printList(list);
+	Node* d = list.tail;
+	while (c != 1)
+	{
+		
+		if ((d->data) % 2 == 0)
+		{
+			del(d);
+			c++;
+			
+		}
+		d = d->p_p;
+	}
+	printList(list);
+
+}
