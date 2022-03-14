e0eb280ed2a65cd3a021b97a9f5cd82fd27af7c0
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2022-03-14 21:59:34 +0500

    Add files via upload

---
+++ Lab 11/Лабораторная работа № 11 Очередь(с библиотекой)  8 вариант .cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	queue <int> q;
+	int n,c=0,c1=0;
+	cout << "Введите длину очереди: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int ch;
+		cin >> ch;
+		if (ch % 2 == 0)
+		{
+			c++;
+		}
+		q.push(ch);
+
+	}
+	while (!q.empty())
+	{
+		if (q.front() % 2==0)
+		{
+			c1++;
+		}
+		if (q.front() % 2 == 0 && c1==c)
+		{
+			q.pop();
+		}
+		else 
+		{
+			cout << q.front();
+			q.pop();
+		}
+	}
+}
