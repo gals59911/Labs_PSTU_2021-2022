7badbb4ca132b4932bff9a544b37aebd6bd8c954
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2022-03-15 15:14:02 +0500

    Add files via upload

---
+++ Lab 11/Лабораторная работа № 11 Очередь(без библиотеки)  8 вариант .cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+using namespace std;
+struct Node {
+	int data;
+	Node* ptr = nullptr;
+};
+struct List {
+	Node* head = nullptr;
+	Node* tail = nullptr;
+};
+void push_back(List& list, int& data)
+{
+	Node* n_n = new Node;
+	n_n->data = data;
+	if (list.head == nullptr)
+	{
+		list.head = n_n;
+		list.tail = n_n;
+	}
+	else {
+		list.tail->ptr = n_n;
+		list.tail = n_n;
+	}
+}
+void del(Node* c_n)
+{
+	c_n= c_n->ptr;
+}
+int main()
+{
+	List list;
+	int n = 10;
+	int c = 0;
+	int c1 = 0;
+	for (int i = 0; i != n; i++)
+	{
+		int s;
+		cin >> s;
+		if (s % 2 == 0)
+		{
+			c++;
+		}
+		push_back(list, s);
+	}
+	Node* c_n = list.head;
+	while (c_n != nullptr)
+	{
+		if (c_n->data % 2 == 0)
+		{
+			c1++;
+		}
+		if (c_n->data % 2 == 0 && c1==c)
+		{
+			del(c_n);
+		}
+		else {
+			cout << c_n->data << " ";
+		}
+		c_n = c_n->ptr;
+
+	}
+}
