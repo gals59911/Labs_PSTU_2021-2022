7aa19915f3c3c2cece4558da73b87ab2a6cb47a0
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2022-03-16 13:42:23 +0500

    Add files via upload

---
+++ Lab 11/Лабораторная работа № 11 Стек(без библиотеки)  8 вариант .cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+struct Node {
+	string l;
+	Node* ptr = nullptr;
+};
+struct List {
+	Node* head = nullptr;
+	Node* tail = nullptr;
+};
+void push_back(List& list, string& l)
+{
+	Node* n_n = new Node;
+	n_n->l = l;
+	if (list.head == nullptr)
+	{
+		list.head = n_n;
+		list.tail = n_n;
+	}
+	else {
+		n_n->ptr = list.head;
+		list.head = n_n;
+	}
+}
+int main()
+{
+	List list;
+	List z;
+	int n = 10;
+	int c = 1;
+	for (int i = 0; i != n; i++)
+	{
+		string s;
+		cin >> s;
+		if (i % 2 == 0)
+		{
+			push_back(z, s);
+		}
+		push_back(list, s); 
+	}
+	
+	Node* c_n = list.head;
+	Node* t = z.head;
+	while (t != nullptr)
+	{
+		cout << t->l << " ";
+		t = t->ptr;
+	}
+	while (c_n != nullptr)
+	{
+		cout << c_n->l << " ";
+		c_n = c_n->ptr;
+	}
+}
