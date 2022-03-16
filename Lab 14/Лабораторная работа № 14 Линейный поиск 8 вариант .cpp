74c21eb565fe0d40ec856650252f0f6caa25ea35
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2022-03-16 21:15:16 +0500

    Add files via upload

---
+++ Lab 14/Лабораторная работа № 14 Линейный поиск 8 вариант .cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+int kol(int mas[],int n, int k)
+{
+	int c = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (mas[i] == k)
+		{
+			n--;
+		}
+	}
+	return n;
+}
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n = 10;
+	int* arr = new int[n];
+	int k;
+	cout << "Введите элемент, который хотите удалить: ";
+	cin >> k;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 10;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	for (int i = 0; i <kol(arr,n,k); i++)
+	{
+		if (arr[i] == k)
+		{
+			arr[i] = arr[i - 1];
+		}
+		else {
+			cout << arr[i] <<" ";
+		}
+		
+	}
+}
